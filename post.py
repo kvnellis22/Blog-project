99e3e5487287f1d14e02aa54313662a37fa5bae7
kvnellis22 <kvnellis22@gmail.com>
2017-01-18 12:15:22 -0600

    Update post.py

--- post.py
+++ post.py
@@ -25,7 +25,7 @@ class Post(db.Model):
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
     last_modified = db.DateTimeProperty(auto_now = True)
-    user_id = db.IntegerProperty(required=True)
+    user_id = db.ReferenceProperty(User)
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
