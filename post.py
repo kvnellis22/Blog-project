b3410174dc8a921eddb622e946c04b17a377a42f
kvnellis22 <kvnellis22@gmail.com>
2017-01-17 19:05:25 -0600

    Create post.py

---
+++ post.py
@@ -0,0 +1,32 @@
+import os
+import re
+import random
+import hashlib
+import hmac
+from string import letters
+
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+
+from user import User
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+    user_id = db.IntegerProperty(required=True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
